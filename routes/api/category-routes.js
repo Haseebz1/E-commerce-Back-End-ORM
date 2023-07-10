c1acbcedd5728c1bf12794b6c6ae8787ba49752c
Haseebz1 <haseebkandore@hotmail.com>
2023-07-10 01:17:58 +0100

    router put method added for update by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,8 +44,28 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+        category_image: req.body.category_image
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!categoryData) {
+        res.status(404).json({message: 'no category found'});
+        return;
+      }
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  
 });
 
 router.delete('/:id', (req, res) => {
