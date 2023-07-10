83a6d5b919c8072902340d8ab6ce9a1ceb88d118
Haseebz1 <haseebkandore@hotmail.com>
2023-07-10 01:21:50 +0100

    delete by id route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,8 +68,23 @@ router.put('/:id', async (req, res) => {
   
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+try {
+  const categoryData = await Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  if (!categoryData) {
+    res.status(404).json({message: 'no category found'});
+    return;
+}
+res.status(200).json(categoryData);
+} catch (err) {
+  res.status(400).json(err);
+}
+
 });
 
 module.exports = router;
